86e65bf5b3a6dd89ebc249f064de3b7098f10398
IonRH <kwxos@vip.qq.com>
2026-09-13 13:49:03 +0800

    手机端重设计：黑白极简风 + 常用话术可编辑 + 消息点击复制

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v3';
+const VERSION = 'v4';
 const SHELL = ['mobile.html', 'manifest.webmanifest', 'icons/icon-192.png', 'icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
